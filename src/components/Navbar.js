d1ffa883332e3d3aaa28c05d3ea6b1b3bd9b2185
Bassam El-Naggar <89227338+BassGI1@users.noreply.github.com>
2022-02-05 20:24:28 -0500

    idk

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav>
 
-            <img src="https://pngimg.com/uploads/trollface/trollface_PNG13.png" className="troll"/>
+            <img src="https://pngimg.com/uploads/trollface/trollface_PNG13.png" className="troll" alt="idk"/>
             <h1 className="title">Meme Generator</h1>
 
         </nav>
